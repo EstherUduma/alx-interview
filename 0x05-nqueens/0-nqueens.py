4b23b2e852301223a76624333077659e301bcb29
Esther <estheruduma23@gmail.com>
2024-07-04 22:33:38 +0100

    correction

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,71 +1,44 @@
 #!/usr/bin/python3
+""" N queens """
 import sys
-"""Solves the nqueens problem"""
 
 
-def is_safe(board, row, col, N):
-    """Document later"""
-    for i in range(row):
-        if board[i] == col or \
-                board[i] - i == col - row or \
-                board[i] + i == col + row:
-            return False
-    return True
+if len(sys.argv) > 2 or len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    exit(1)
+
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
+
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
+
+n = int(sys.argv[1])
 
 
-def solve_nqueens_util(board, row, N):
-    """Document later"""
-    if row == N:
-        return [board[:]]
-
-    solutions = []
-    for col in range(N):
-        if is_safe(board, row, col, N):
-            board[row] = col
-            solutions += solve_nqueens_util(board, row + 1, N)
-            board[row] = -1
-
-    return solutions
+def queens(n, i=0, a=[], b=[], c=[]):
+    """ find possible positions """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
 
 
-def solve_nqueens(N):
-    """Document later"""
-    if not isinstance(N, int):
-        print("N must be a number")
-        sys.exit(1)
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-    board = [-1] * N
-    solutions = solve_nqueens_util(board, 0, N)
-    if not solutions:
-        print("No solution exists")
-        return
-
-    for sol in solutions:
-        print_solution(sol)
+def solve(n):
+    """ solve """
+    k = []
+    i = 0
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
 
 
-def print_solution(board):
-    """Document later"""
-    print("[", end="")
-    for i, col in enumerate(board):
-        print(f"[{i}, {col}]", end="")
-        if i != len(board) - 1:
-            print(", ", end="")
-    print("]", end=" ")
-    print()
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
-    try:
-        N = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-    solve_nqueens(N)
+solve(n)
