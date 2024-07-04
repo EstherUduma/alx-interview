bf1eb8ff67c0667f9463e0bfcd7cbdd73cad3568
Esther <estheruduma23@gmail.com>
2024-07-04 23:08:34 +0100

    again

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,71 +1,54 @@
 #!/usr/bin/python3
+"""
+N queens
+"""
+
 import sys
-"""Solves the nqueens problem"""
+
+if len(sys.argv) != 2:
+    print('Usage: nqueens N')
+    exit(1)
+
+try:
+    n_q = int(sys.argv[1])
+except ValueError:
+    print('N must be a number')
+    exit(1)
+
+if n_q < 4:
+    print('N must be at least 4')
+    exit(1)
 
 
-def is_safe(board, row, col, N):
-    """Document later"""
-    for i in range(row):
-        if board[i] == col or \
-                board[i] - i == col - row or \
-                board[i] + i == col + row:
+def solve_nqueens(n):
+    '''self descriptive'''
+    if n == 0:
+        return [[]]
+    inner_solution = solve_nqueens(n - 1)
+    return [solution + [(n, i + 1)]
+            for i in range(n_q)
+            for solution in inner_solution
+            if safe_queen((n, i + 1), solution)]
+
+
+def attack_queen(square, queen):
+    '''self descriptive'''
+    (row1, col1) = square
+    (row2, col2) = queen
+    return (row1 == row2) or (col1 == col2) or\
+        abs(row1 - row2) == abs(col1 - col2)
+
+
+def safe_queen(sqr, queens):
+    '''self descriptive'''
+    for queen in queens:
+        if attack_queen(sqr, queen):
             return False
     return True
 
 
-def solve_nqueens_util(board, row, N):
-    """Document later"""
-    if row == N:
-        return [board[:]]
-
-    solutions = []
-    for col in range(N):
-        if is_safe(board, row, col, N):
-            board[row] = col
-            solutions += solve_nqueens_util(board, row + 1, N)
-            board[row] = -1
-
-    return solutions
-
-
-def solve_nqueens(N):
-    """Document later"""
-    if not isinstance(N, int):
-        print("N must be a number")
-        sys.exit(1)
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-    board = [-1] * N
-    solutions = solve_nqueens_util(board, 0, N)
-    if not solutions:
-        print("No solution exists")
-        return
-
-    for sol in solutions:
-        print_solution(sol)
-
-
-def print_solution(board):
-    """Document later"""
-    print("[", end="")
-    for i, col in enumerate(board):
-        print(f"[{i}, {col}]", end="")
-        if i != len(board) - 1:
-            print(", ", end="")
-    print("]", end=" ")
-    print()
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
-    try:
-        N = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-    solve_nqueens(N)
+for answer in reversed(solve_nqueens(n_q)):
+    result = []
+    for p in [list(p) for p in answer]:
+        result.append([i - 1 for i in p])
+    print(result)
